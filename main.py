8f61198a223817c317cf6559c22ae65e68d474a3
koplercito <kopler201x@gmail.com>
2024-02-16 19:27:50 -0600

    a01

---
+++ main.py
@@ -0,0 +1,13 @@
+from pygame import *
+import pygame as pg
+import random
+
+#Determinar los usos del inicio e interfaz grafica
+class Inicio:
+    def __init__(self):
+        self.main_theme
+    pass
+
+
+random.randrange[1, 6]
+
